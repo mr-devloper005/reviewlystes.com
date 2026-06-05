222a4fbda4d991a5c26f5e43fb13158f580eea47
mr-devloper005 <mr.opxoo5@gmail.com>
2026-06-05 23:01:45 +0530

    Restrict enabled tasks to allotted site type

--- src/editable/content/tasks.config.ts
+++ src/editable/content/tasks.config.ts
@@ -2,12 +2,12 @@ import type { TaskKey } from "@/lib/site-config";
 
 export const slot4TaskSupport = {
   article: true,
-  classified: true,
-  sbm: true,
-  profile: true,
-  pdf: true,
-  listing: true,
-  image: true,
+  classified: false,
+  sbm: false,
+  profile: false,
+  pdf: false,
+  listing: false,
+  image: false,
 } satisfies Record<TaskKey, boolean>;
 
 export const slot4TaskNotes = {
